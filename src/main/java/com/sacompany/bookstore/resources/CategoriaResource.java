a5dad92a8acc5d7b7f58ee8ff2a068724685bee1
sealexsandro <sealexsandro@gmail.com>
2021-03-01 09:21:24 -0300

    finAll para a Categoria utilizando o padrão DTO

--- src/main/java/com/sacompany/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/sacompany/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,9 @@
 package com.sacompany.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sacompany.bookstore.domain.Categoria;
+import com.sacompany.bookstore.dtos.CategoriaDTO;
 import com.sacompany.bookstore.service.CategoriaService;
 
 @RestController
@@ -21,5 +26,12 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/sacompany/bookstore/service/CategoriaService.java
+++ src/main/java/com/sacompany/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.sacompany.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto Não Encontrado! Id: " + id + ", Tipo = " + Categoria.class.getName()));
 
+	}
+	
+	public List<Categoria> findAll(){
+		return categoriaRepository.findAll();
 	}
 }
